4a2f675097bdfacecb94f59ba2bdbbf854d764f0
Marc Lowenstein <marc.lowenstein@gmail.com>
2016-05-28 21:07:40 +0200

    Alle classes toegevoegd

---
+++ AirBnB/Controllers/Betaling.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirBnB.Controllers
+{
+    public class Betaling
+    {
+        public int Id { get; set; }
+        public DateTime BetalingsDatum { get; set; }
+        public Double Bedrag { get; set; }
+        public Reservering Reservering { get; set; }
+    }
+}
