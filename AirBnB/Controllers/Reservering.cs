0e29dad010a43d883f1aaf934468fa108481f83c
Marc Lowenstein <marc.lowenstein@gmail.com>
2016-05-30 10:39:57 +0200

    Reservering aangepast

--- AirBnB/Controllers/Reservering.cs
+++ AirBnB/Controllers/Reservering.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,15 @@ namespace AirBnB.Controllers
     public class Reservering
     {
         public int Id { get; set; }
+        [Required]
         public string AirBnbCode { get; set; }
+        [Required]
         public DateTime IncheckDatum { get; set; }
+        [Required]
         public int AantalNachten { get; set; }
+        [Required]
         public int AantalPersonen { get; set; }
+        [Required]
         public double TotalePrijs { get; set; }
     }
 }
